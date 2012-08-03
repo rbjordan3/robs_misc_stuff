3182f8bbf954c5bd6d75345af0d007f1159e2d1f
rbjordan3 <rob@glideint.com>
2012-08-03 10:00:19 -0700

    added more formated versions

--- password_gen.pl
+++ password_gen.pl
@@ -24,6 +24,10 @@ my @shuffled_words = shuffle(@words);
 
 my $word1 =  $shuffled_words[int(rand(10)*100)];
 my $word2 =  $shuffled_words[int(rand(10)*100)];
+my $number =  int(rand(10)*10);
+my $symbol = $symbols[rand(10)*10 % 11];
+
+printf "%s%2d%s%s\n", $word1, int(rand(10)*10),  $word2, $symbols[rand(10)*10 % 11];
 
 if ($change) {
     $word1 =~ s/[iI]/1/g;
@@ -34,10 +38,15 @@ if ($change) {
 
     $word1 =~ s/[sS]/\$/g;
     $word2 =~ s/[sS]/\$/g;
-    
+
+#    $word1 =~ tr/^[a-z]*/[A-Z]/;
+#    $word2 =~ tr/^[a-z]*/[A-Z]/;
 }
 
 
 
-printf "%s%2d%s%s\n", $word1, int(rand(10)*10),  $word2, $symbols[rand(10)*10 % 11];
-
+print "Possible Passwords:\n";
+printf "%s%02d%s%s\n", $word1, $number,  $word2, $symbol;
+printf "%2d%s%s%s\n", $number, $word1,  $word2, $symbol;
+printf "%s%02d%s%s\n", $word1, $number, $symbol, $word2;
+printf "%s%s%s%02d\n", $word1, $symbol, $word2, $number;
